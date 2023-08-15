8e81537121939e48a45c3ced37d4ae01a71f3353
talukder9712 <talukder9712@gmail.com>
2023-08-15 18:00:13 -0400

    remove account

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,30 +63,30 @@ module "sandbox" {
 #   account_customizations_name = "network"
 # }
 
-module "sandbox1" {
-  source = "./modules/aft-account-request"
+# module "sandbox1" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "nusaybah0412+sanbox1@gmail.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "nusaybah0412+sanbox1@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "nusaybah0412+sanbox1@gmail.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "nusaybah0412+sanbox1@gmail.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
