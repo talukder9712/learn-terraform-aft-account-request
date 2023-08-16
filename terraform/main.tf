e6c1766b6acbdb53bc701e4c948a1f7b03520141
talukder9712 <talukder9712@gmail.com>
2023-08-15 20:15:17 -0400

    add sandbox4

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,60 +63,33 @@ module "network" {
   account_customizations_name = "network"
 }
 
-# module "sandbox1" {
-#   source = "./modules/aft-account-request"
-
-#   control_tower_parameters = {
-#     AccountEmail              = "nusaybah0412+sanbox1@gmail.com"
-#     AccountName               = "sandbox-aft1"
-#     ManagedOrganizationalUnit = "Sandbox"
-#     SSOUserEmail              = "nusaybah0412+sanbox1@gmail.com"
-#     SSOUserFirstName          = "Sandbox"
-#     SSOUserLastName           = "AFT1"
-#   }
-
-#   account_tags = {
-#     "Learn Tutorial" = "AFT1"
-#   }
-
-#   change_management_parameters = {
-#     change_requested_by = "HashiCorp Learn"
-#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-#   }
-
-#   custom_fields = {
-#     group = "non-prod"
-#   }
-
-#   account_customizations_name = "sandbox1"
-# }
 
 
+module "sandbox2" {
+  source = "./modules/aft-account-request"
 
-# module "sandbox1" {
-#   source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "nusaybah0412+sanbox12@gmail.com"
+    AccountName               = "sandbox-aft12"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "nusaybah0412+sanbox12@gmail.com"
+    SSOUserFirstName          = "Sandbox12"
+    SSOUserLastName           = "AFT12"
+  }
 
-#   control_tower_parameters = {
-#     AccountEmail              = "nusaybah0412+sanbox12@gmail.com"
-#     AccountName               = "sandbox-aft12"
-#     ManagedOrganizationalUnit = "Sandbox"
-#     SSOUserEmail              = "nusaybah0412+sanbox12@gmail.com"
-#     SSOUserFirstName          = "Sandbox12"
-#     SSOUserLastName           = "AFT12"
-#   }
+  account_tags = {
+    "Learn Tutorial" = "AFT12"
+  }
 
-#   account_tags = {
-#     "Learn Tutorial" = "AFT12"
-#   }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
 
-#   change_management_parameters = {
-#     change_requested_by = "HashiCorp Learn"
-#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-#   }
+  custom_fields = {
+    group = "non-prod"
+  }
 
-#   custom_fields = {
-#     group = "non-prod"
-#   }
+  account_customizations_name = "sandbox12"
+}
 
-#   account_customizations_name = "sandbox12"
-# }
